ed94f96a4e3e001abe33f3eed485144371c129d4
hmzshin <hamzasahin@outlook.com>
2023-11-01 19:21:21 +0300

    fetch data from the api using axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((response) => {
+        console.log("fetched data", response.data.data);
+        setUser(response.data.data);
+      })
+      .catch((error) => {
+        alert("Can not reach server", error);
+      })
+      .finally(() => {
+        console.log("fetch request is completed");
+      });
+  }, []);
+
   return (
     <div className="App">
       <LoginForm />
