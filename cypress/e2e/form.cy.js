65f18d2f93296f82ec11f47abf39423ffeb6d982
hmzshin <hamzasahin@outlook.com>
2023-11-02 23:21:20 +0300

    give class name to elements to use in test

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -6,42 +6,75 @@ describe("User Log in Validation Tests", () => {
   });
 
   it("enter a valid name", () => {
-    cy.get("#form_name_area").type("Na");
+    cy.get(".form_name_area").type("Lorem");
+    cy.get(".form_name_area_validty").should("not.be.visible");
 
-    cy.get("#form_name_area_validty")
+    cy.get(".form_name_area").type(
+      "{backspace}{backspace}{backspace}{backspace}{backspace}"
+    );
+
+    cy.get(".form_name_area").type("Na");
+
+    cy.get(".form_name_area_validty")
       .should("be.visible")
       .should("have.text", "Can not be less then 3 characters");
 
-    cy.get("#form_name_area").type("{backspace}{backspace}");
+    cy.get(".form_name_area").type("{backspace}{backspace}");
 
-    cy.get("#form_name_area_validty")
+    cy.get(".form_name_area_validty")
       .should("be.visible")
       .should("have.text", "Can not be empty");
 
-    cy.get("#form_name_area").type("Lorem ipsum dolor sit amet ");
+    cy.get(".form_name_area").type("Lorem ipsum dolor sit amet ");
 
-    cy.get("#form_name_area_validty")
+    cy.get(".form_name_area_validty")
       .should("be.visible")
       .should("have.text", "Can not be more then 20 characters");
   });
 
   it("enter a valid surname", () => {
-    cy.get("#form_surname_area").type("Su");
+    cy.get(".form_surname_area").type("Lorem");
+    cy.get(".form_surname_area_validty").should("not.be.visible");
 
-    cy.get("#form_surname_area_validty")
+    cy.get(".form_surname_area").type(
+      "{backspace}{backspace}{backspace}{backspace}{backspace}"
+    );
+
+    cy.get(".form_surname_area").type("Su");
+
+    cy.get(".form_surname_area_validty")
       .should("be.visible")
       .should("have.text", "Can not be less then 3 characters");
 
-    cy.get("#form_surname_area").type("{backspace}{backspace}");
+    cy.get(".form_surname_area").type("{backspace}{backspace}");
 
-    cy.get("#form_surname_area_validty")
+    cy.get(".form_surname_area_validty")
       .should("be.visible")
       .should("have.text", "Can not be empty");
 
-    cy.get("#form_surname_area").type("Lorem ipsum dolor sit amet ");
+    cy.get(".form_surname_area").type("Lorem ipsum dolor sit amet ");
 
-    cy.get("#form_surname_area_validty")
+    cy.get(".form_surname_area_validty")
       .should("be.visible")
       .should("have.text", "Can not be more then 20 characters");
+  });
+  it("enter a valid email", () => {
+    cy.get(".form_email_input").type("Lorem");
+
+    cy.get(".form_email_validty")
+      .should("be.visible")
+      .should("have.text", "Invalid email!");
+
+    cy.get(".form_email_input").type(
+      "{backspace}{backspace}{backspace}{backspace}{backspace}"
+    );
+
+    cy.get(".form_email_validty")
+      .should("be.visible")
+      .should("have.text", "Can not be empty");
+
+    cy.get(".form_email_input").type("Lorem@example.com");
+
+    cy.get(".form_email_validty").should("not.be.visible");
   });
 });
